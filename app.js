dbee0dc4288b8fa1ac43d23f844f685a76f0867d
HamzaJirah <hamzajirah@gmail.com>
2019-05-17 16:59:18 +0100

    selected DOM element

---
+++ app.js
@@ -0,0 +1,8 @@
+// Get DOM element
+const playerAScore = document.querySelector('.player-a-score');
+const playerBScore = document.querySelector('.player-b-score');
+const scoreMessage = document.querySelector('.score-message > p')
+console.log(scoreMessage);
+
+
+
