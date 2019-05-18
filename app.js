1aaf073da38f65b9ed4563b65f1de8ee4003c54c
HamzaJirah <hamzajirah@gmail.com>
2019-05-18 15:53:03 +0100

    returned the result of the balls iteration

--- app.js
+++ app.js
@@ -6,15 +6,17 @@ const balls = document.getElementsByTagName('img');
 
 // Loop through balls and Add event listener
 function loopThroughBalls(){
+  let allBalls;
   for(let i = 0; i < balls.length; i++){
-    balls[i].addEventListener('click', playGame);
+    allBalls = balls[i].addEventListener('click', playGame);
   }
+  return allBalls;
 }
-loopThroughBalls();
+ loopThroughBalls();
 
 // playGame 
 function playGame(){
-  
+
 }
 
 
