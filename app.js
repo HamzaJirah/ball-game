9047152d16d07608711e1bb878b6df77270fad84
HamzaJirah <hamzajirah@gmail.com>
2019-05-18 15:39:04 +0100

    added click event to balls

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const playerBScore = document.querySelector('.player-b-score');
 const scoreMessage = document.querySelector('.score-message > p');
 const balls = document.getElementsByTagName('img');
 
+// Add event listener to balls
+balls.addEventListener('click', playGame);
 for(let i = 0; i < balls.length; i++){
   console.log(balls[i]);
 }
