1dc8b7953fdd470881e8552c152afe33a2a4d3b8
HamzaJirah <hamzajirah@gmail.com>
2019-05-18 15:24:50 +0100

    added pseudo-code to generate and assign random numbers

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 // Get DOM element
 const playerAScore = document.querySelector('.player-a-score');
 const playerBScore = document.querySelector('.player-b-score');
-const scoreMessage = document.querySelector('.score-message > p')
-console.log(scoreMessage);
+const scoreMessage = document.querySelector('.score-message > p');
+const balls = document.get('ball');
+console.log(balls);
 
 
 
