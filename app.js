027496c7d8577302f6283edbb14fed3577d30ce4
HamzaJirah <hamzajirah@gmail.com>
2019-05-18 15:34:46 +0100

    iterate through the balls

--- app.js
+++ app.js
@@ -2,8 +2,10 @@
 const playerAScore = document.querySelector('.player-a-score');
 const playerBScore = document.querySelector('.player-b-score');
 const scoreMessage = document.querySelector('.score-message > p');
-const balls = document.get('ball');
-console.log(balls);
+const balls = document.getElementsByTagName('img');
 
+for(let i = 0; i < balls.length; i++){
+  console.log(balls[i]);
+}
 
 
