665c716b9914f3bdec7f6b2c3dba4c7295722f77
HamzaJirah <hamzajirah@gmail.com>
2019-05-19 14:04:18 +0100

    stored the result of player A and B scores in vairables

--- app.js
+++ app.js
@@ -22,17 +22,17 @@ function playGame(){
       let playerA_BallB = Math.floor(Math.random(balls[1]) * 11);
       let playerA_BallC = Math.floor(Math.random(balls[2]) * 11);
       // playerA random scores
-      playerAScore.textContent = playerA_BallA;
-      playerAScore.textContent = playerA_BallB;
-      playerAScore.textContent = playerA_BallC;
+      let playerA_Ball_A_Score = playerAScore.textContent = playerA_BallA;
+      let playerA_Ball_B_Score = playerAScore.textContent = playerA_BallB;
+      let playerA_Ball_C_Score = playerAScore.textContent = playerA_BallC;
       // generate random numbers for every ball selection by Player B
       let playerB_BallA = Math.floor(Math.random(balls[0]) * 11);
       let playerB_BallB = Math.floor(Math.random(balls[1]) * 11);
       let playerB_BallC = Math.floor(Math.random(balls[2]) * 11);
       // playerA random scores
-      playerBScore.textContent = playerB_BallA;
-      playerBScore.textContent = playerB_BallB;
-      playerBScore.textContent = playerB_BallC; 
+      let playerB_Ball_A_Score = playerBScore.textContent = playerB_BallA;
+      let playerB_Ball_B_Score = playerBScore.textContent = playerB_BallB;
+      let playerB_Ball_C_Score = playerBScore.textContent = playerB_BallC; 
   }
 }
 playGame();
