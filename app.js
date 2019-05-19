27742589b5c3a143d64119513648f5b7032898b4
HamzaJirah <hamzajirah@gmail.com>
2019-05-19 14:56:23 +0100

    added comment to compare players score

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ function playGame(){
       let playerA_BallB = Math.floor(Math.random(balls[1]) * 11);
       let playerA_BallC = Math.floor(Math.random(balls[2]) * 11);
       // playerA random scores from the three balls
-      let playerA_Ball_A_Score = playerAScore.textContent = playerA_BallA;
+      let playerA_Ball_A_Score = playerAScore.textContent = Number(playerA_BallA);
       let playerA_Ball_B_Score = playerAScore.textContent = playerA_BallB;
       let playerA_Ball_C_Score = playerAScore.textContent = playerA_BallC;
       // generate random numbers for every ball selection by Player B
@@ -32,7 +32,10 @@ function playGame(){
       // playerA random scores from the three balls
       let playerB_Ball_A_Score = playerBScore.textContent = playerB_BallA;
       let playerB_Ball_B_Score = playerBScore.textContent = playerB_BallB;
-      let playerB_Ball_C_Score = playerBScore.textContent = playerB_BallC; 
+      let playerB_Ball_C_Score = playerBScore.textContent = playerB_BallC;
+      // Compare player A and player B scores when Ball A is selected
+      
+      
   }
 }
 playGame();
